78073b0fa3b8c720f34b6a4fb8770e94e9874c5a
yazid113 <92461753+yazid113@users.noreply.github.com>
2021-12-12 23:52:22 -0500

    Update script.js

--- interactive-form-v3/js/script.js
+++ interactive-form-v3/js/script.js
@@ -90,11 +90,11 @@ $($form).submit(function (e) {
     const $nameValue = $nameField.val()
     const regexName = /^[a-z ,.'-]+$/i
     const $emailValue = $emailField.val()
-    const regexEmail = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
-
-    if (!regexName.test($nameValue) && !regexEmail.test($emailValue)) {
+    const regexEmail = /^[^@]+@[^@.]+\.[a-z]+$/i
+    if (!regexName.test($nameValue) || !regexEmail.test($emailValue)) {
         e.preventDefault();
     }
    // console.log(regexObjecct.test($nameValue))
-    
+   //console.log(!regexEmail.test($emailValue))
+   //e.preventDefault();
 });
